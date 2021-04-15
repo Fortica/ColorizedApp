3b2d52043f8bdac0b15047c7aaf92d662c093ecf
Galatea <marcinevskaya93@mail.ru>
2021-04-15 20:43:43 +0300

    Initial Commit

---
+++ HW 2/ViewControllers/ColorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ColorViewController.swift
+//  HW 2
+//
+//  Created by Larisa on 15.04.2021.
+//  Copyright Larisa Marcinevskaya. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .black
+        
+    }
+    
+
+
+
+}
